efff4be60e0ce34054c3946182e8a8d0383778f3
farhad-ahmadi-04 <farhad.13ahmadi.83@gmail.com>
2024-03-09 19:00:14 +0330

    feat (dacument)
    Des :
    + add json file for set dacument  of projects

---
+++ js/index.js
@@ -0,0 +1,41 @@
+/* <div class="project-item">
+    <img src="./images/image04.jfif" alt="">
+    <h5 class="title">mini Library</h5>
+    <p class="text">mini library</p>
+    <a href="" class="project-link">visite</a>
+ </div> */
+const projects = document.querySelector('#projects')
+
+function prodactItem(image, title, text) {
+    // create div
+    const prodact = document.createElement('div');
+    prodact.classList.add('project-item')
+
+    // create image
+    const img = document.createElement('img')
+    img.setAttribute('src', image)
+    prodact.appendChild(img)
+
+    // create h5
+    const pTitle = document.createElement('h5')
+    pTitle.classList.add('title')
+    pTitle.textContent = title
+    prodact.appendChild(pTitle)
+
+
+    // create paragraph
+    const paragraph = document.createElement('p')
+    paragraph.classList.add('text')
+    paragraph.textContent = text
+    prodact.appendChild(paragraph)
+
+    // create link
+    const link = document.createElement('a')
+    link.classList.add('project-link')
+    link.textContent = 'visite'
+    prodact.appendChild(link)
+
+    return prodact
+}
+
+projects.appendChild(prodactItem('./images/image04.jfif', 'mini Library', 'mini library'))
