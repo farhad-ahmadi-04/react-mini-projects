a02886a3b7ccfb8cdc9415276dd0e08b7502d3a3
farhad-ahmadi-04 <farhad.13ahmadi.83@gmail.com>
2024-03-12 20:50:04 +0330

    feat

--- js/module/gettingData.js
+++ js/module/gettingData.js
@@ -6,6 +6,6 @@
  * get data from json file and pars it and in the end return the data
  */
 async function data() {
-    return await (await (fetch("./js/module/data.JSON"))).json();
+    return await (await (fetch("./data.json"))).json();
 }
 export default data
